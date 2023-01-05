d6f478cce99e6a2ce57584432c318eaa1512fafc
jonathanchang <jonathan.chang@livibank.com>
2023-01-05 14:14:26 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import Script from "next/script";
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -8,9 +9,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <script src="https://livi-poc-widget1.vercel.app/_next/static/chunks/remoteEntry.js" />
-        <script src="https://livi-poc-widget2.vercel.app/_next/static/chunks/remoteEntry.js" />
-        <script src="https://livi-poc-widget3.vercel.app/_next/static/chunks/remoteEntry.js" />
+        <Script src="https://livi-poc-widget1.vercel.app/_next/static/chunks/remoteEntry.js" />
+        <Script src="https://livi-poc-widget2.vercel.app/_next/static/chunks/remoteEntry.js" />
+        <Script src="https://livi-poc-widget3.vercel.app/_next/static/chunks/remoteEntry.js" />
         <Head />
         <body>
           <Main />
