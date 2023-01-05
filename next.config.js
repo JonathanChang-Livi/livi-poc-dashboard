2ede2d28ad2a57777f11b5eb37a8722cf5420262
jonathanchang <jonathan.chang@livibank.com>
2023-01-06 03:31:00 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -16,7 +16,7 @@ const nextConfig = {
         },
         filename: 'static/chunks/remoteEntry.js',
         exposes: {
-          './dashboard': './component/dashboard',
+          './demo': './component/dashboard',
           './widgets': './component/widget.tsx',
         },
         shared: {
